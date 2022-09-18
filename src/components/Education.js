bcba87c688b23589595d561d8640bbe19eae06cf
JacobGuiang <jacob.guiang1@gmail.com>
2022-09-18 00:00:20 -0500

    Create components directory and files

---
+++ src/components/Education.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-useless-constructor */
+import React, { Component } from 'react';
+
+class Education extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return <div>Hello, world!</div>;
+  }
+}
+
+export default Education;
---
+++ src/components/Experience.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-useless-constructor */
+import React, { Component } from 'react';
+
+class Experience extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return <div>Hello, world!</div>;
+  }
+}
+
+export default Experience;
